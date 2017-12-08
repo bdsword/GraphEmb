c41b15b923958d22b25ab273d1b6d120348c6d22
BDSword <thetruebloodsword@gmail.com>
2017-12-08 13:52:14 +0800

    Add utils.py which contains _shart_shell function.

---
+++ utils.py
@@ -0,0 +1,9 @@
+def _start_shell(local_ns=None):
+    # An interactive shell is useful for debugging/development.
+    import IPython
+    user_ns = {}
+    if local_ns:
+        user_ns.update(local_ns)
+        user_ns.update(globals())
+        IPython.start_ipython(argv=[], user_ns=user_ns)
+
