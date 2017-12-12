1b98aacef7f8c5282037cc081b1fe1b704010a69
BDSword <thetruebloodsword@gmail.com>
2017-12-12 13:02:53 +0800

    Add check_arch to check x86 arch.

--- attributes.py
+++ attributes.py
@@ -1,7 +1,17 @@
 
 import re
 
+
+
+def check_arch(unknown_arch):
+    available_archs = ['arm', 'x86']
+    for arch in available_archs:
+        if arch in unknown_arch:
+            return arch
+    return unknown_arch
+
 def num_calls(code, arch):
+    arch = check_arch(arch)
     arch_ins_pattern = {'arm': r'bl\s+.*$', 'x86': r'call\s+.*$'}
     counter = 0
     code = code.replace('\\l', '\n')
@@ -13,6 +23,7 @@ def num_calls(code, arch):
 
 
 def num_instructions(code, arch):
+    arch = check_arch(arch)
     code = code.replace('\\l', '\n')
     code = re.sub(r'^;.*$', r'', code)
     instructions = re.findall(r'\s*0x[0-9a-f]{8}\s+(.*)', code)
@@ -20,6 +31,7 @@ def num_instructions(code, arch):
 
 
 def num_transfer(code, arch):
+    arch = check_arch(arch)
     arch_ins_pattern = {'arm': r'(mov|mvn|ldr[b|h|sb|sh]{0,1}|str[b|h|sb|sh]{0,1}|ldm[ia|ib|da|db]{0,1}|stm[ia|ib|da|db]{0,1}|swp[b]{0,1}|push|pop)\s+.*', 'x86': r'(mov|xchg|cmpxchg|movz|movzx|movs|movsx|movsb|movsw|lea|pusha|popa|pushad|popad|lds|les|pushf|popf|lahf|sahf|in|out|push|pop)\s+.*'}
     counter = 0
     code = code.replace('\\l', '\n')
@@ -31,6 +43,7 @@ def num_transfer(code, arch):
 
 
 def num_arithmetic(code, arch):
+    arch = check_arch(arch)
     arch_ins_pattern = {'arm': r'(add|adc|sub|sbc|rsb|rsc|and|eor|orr|bic|lsl|lsr|asr|ror|rrx)\s+.*', 'x86': r'(add|sub|inc|dec|imul|idiv|and|or|xor|not|neg|shl|shr)\s+.*'}
     counter = 0
     code = code.replace('\\l', '\n')
