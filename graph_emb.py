2974a70f0976771c27a6f845ed1f9acad3b1b0ef
BDSword <thetruebloodsword@gmail.com>
2017-12-12 15:44:46 +0800

    Finish rebuild graph embedding for every pair.

--- graph_emb.py
+++ graph_emb.py
@@ -7,9 +7,11 @@ import numpy as np
 from utils import _start_shell
 from structures import Graph, Node, Edge
 import pickle
+from random import shuffle
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 
 T = 5 
 relu_layer_num = 3
@@ -24,34 +26,16 @@ def simga_function(input_l_v, P_n):
     return output
 
 
-def graph_emb(graphs):
-    # Graph:
-    #   nodes: {id, code, neighbors}
-    #   edges: {src, dest}
+def graph_emb(graph, W1, W2, P_n):
     with tf.device('/gpu:0'):
-        x_num_nodes = tf.placeholder(tf.int32)
-        y_num_nodes = tf.placeholder(tf.int32)
-        x_neighbors = tf.placeholder(tf.int32)
-        y_neighbors = tf.placeholder(tf.int32)
-
-        # Static parameters which are shared by each cfg 
-        W1 = tf.get_variable("W1", [ACFG_node_dim, emb_size], initializer=tf.random_normal_initializer()) # d x p
-        W2 = tf.get_variable("W2", [emb_size, emb_size], initializer=tf.random_normal_initializer()) # p x p
-        P_n = []
-        for _ in range(relu_layer_num):
-            P_n.append(tf.Variable(tf.random_normal([emb_size, emb_size])))
-
-
         graphs_embs = []
-        # Build graphs for each cfg
-        for idx, graph in enumerate(graphs):
-            # Dynamic parameters for each cfg
-            u_v = tf.Variable(tf.zeros([len(graph.nodes), emb_size]), name="u_v_{}".format(idx))
+        u_v = tf.Variable(tf.zeros([len(graph.nodes), emb_size]), name="u_v")
 
-            for t in range(T):
-                u_v_t_list = [None] * len(graph.nodes)
-                for v in graph.nodes:
-                    neighbor_v = list(v.neighbors)
+        for t in range(T):
+            u_v_t_list = [None] * len(graph.nodes)
+            for v in graph.nodes:
+                neighbor_v = list(v.neighbors)
+                if len(neighbor_v) > 0:
                     neighbor_u = tf.nn.embedding_lookup(u_v, neighbor_v)
                     l_v = tf.reshape(tf.reduce_sum(neighbor_u, 0), [-1, emb_size])
 
@@ -66,58 +50,91 @@ def graph_emb(graphs):
                                 )
                             )
                     u_v_t_list[v.node_id] = tf.transpose(u_v_t)
-                u_v = tf.reshape(tf.stack(u_v_t_list), [-1, emb_size])
-            graph_emb = tf.matmul(W2, tf.reshape(tf.reduce_sum(u_v, 0), [-1, emb_size]), transpose_b=True)
-            graphs_embs.append(graph_emb)
+                else:
+                    u_v_t_list[v.node_id] = tf.nn.embedding_lookup(u_v, v.node_id)
+            u_v = tf.reshape(tf.stack(u_v_t_list), [-1, emb_size])
+        graph_emb = tf.matmul(W2, tf.reshape(tf.reduce_sum(u_v, 0), [-1, emb_size]), transpose_b=True)
+        graphs_embs.append(graph_emb)
     return tf.reshape(tf.stack(graphs_embs), [-1, emb_size])
 
 
+def shuffle_data(dataset):
+    data_size = len(dataset['sample'])
+    idx_list = [i for i in range(data_size)]
+    shuffle(idx_list)
+    return np.asarray(dataset['sample'])[idx_list], np.asarray(dataset['label'])[idx_list]
+
+
 def main(argv):
+    if len(argv) != 2:
+        print('Usage:\n\tgraph_emb.py <train pickle data>')
+        sys.exit(-1)
 
     with open(sys.argv[1], 'rb') as f:
         learning_data = pickle.load(f)
 
-    f_win = open(argv[1], 'rb')
-    f_linux = open(argv[2], 'rb')
-    f_arm = open(argv[3], 'rb')
-    
-    win_graph = pickle.load(f_win)
-    linux_graph = pickle.load(f_linux)
-    arm_graph = pickle.load(f_arm)
-    
-    graphs = [win_graph, linux_graph, arm_graph]
+    cur_epoch = 0
+    num_epoch = 1000
+    W1_val = None
+    W2_val = None
+    P_n_val = None
 
-    with tf.variable_scope("siamese") as scope:
-        x_graph_id = tf.placeholder(tf.int32)
-        y_graph_id = tf.placeholder(tf.int32)
-        label = tf.placeholder(tf.int32)
-        graphs_embs = graph_emb(graphs)
+    while cur_epoch < num_epoch:
+        samples, labels = shuffle_data(learning_data['train'])
+        cur_step = 0
+        epoch_loss = 0
+        real_loss = epoch_loss / len(samples)
+        for sample, label in zip(samples, labels):
+            with tf.device('/gpu:0'):
+                if W1_val is None:
+                    W1 = tf.Variable(tf.random_normal([ACFG_node_dim, emb_size]), name="W1") # d x p
+                else:
+                    W1 = tf.Variable(W1_val, name="W1")
 
-        x = tf.nn.embedding_lookup(graphs_embs, [x_graph_id])
-        y = tf.nn.embedding_lookup(graphs_embs, [y_graph_id])
+                if W2_val is None:
+                    W2 = tf.Variable(tf.random_normal([emb_size, emb_size]), name="W2") # p x p
+                else:
+                    W2 = tf.Variable(W2_val, name="W2")
 
-        x = tf.transpose(x)
-        y = tf.transpose(y)
+                P_n = []
+                if P_n_val is None:
+                    for _ in range(relu_layer_num):
+                        P_n.append(tf.Variable(tf.random_normal([emb_size, emb_size])))
+                else:
+                    for P in P_n_val:
+                        P_n.append(tf.Variable(P))
 
-        norm_x = tf.nn.l2_normalize(x, 0)
-        norm_y = tf.nn.l2_normalize(y, 0)
-        cos_similarity = tf.reduce_sum(tf.multiply(norm_x, norm_y))
-        loss_op = tf.square(cos_similarity - label)
-        train_op = tf.train.GradientDescentOptimizer(0.03).minimize(loss_op)
-        init_op = tf.global_variables_initializer()
 
-    with tf.Session() as sess:
-        sess.run(init_op)
-        for _ in range(1000):
-            _, loss, x_val, y_val = sess.run([train_op, loss_op, x, y],
-                               {x_graph_id: 0, y_graph_id: 2})
-            print('loss: {}'.format(loss))
+
+            x = graph_emb(sample[0], W1, W2, P_n)
+            y = graph_emb(sample[1], W1, W2, P_n)
+
+            x = tf.transpose(x)
+            y = tf.transpose(y)
+
+            norm_x = tf.nn.l2_normalize(x, 0)
+            norm_y = tf.nn.l2_normalize(y, 0)
+            cos_similarity = tf.reduce_sum(tf.multiply(norm_x, norm_y))
+            loss_op = tf.square(cos_similarity - label)
+            train_op = tf.train.GradientDescentOptimizer(0.03).minimize(loss_op)
+            init_op = tf.global_variables_initializer()
+
+            with tf.Session() as sess:
+                sess.run(init_op)
+                num_repeat = 10
+                loss_sum = 0
+                for _ in range(num_repeat):
+                    _, loss = sess.run([train_op, loss_op])
+                    loss_sum += loss
+                sys.stdout.write('Epoch: {:10}, Loss: {:15.10f}, Step: {:10}     \r'.format(cur_epoch, real_loss, cur_step))
+                sys.stdout.flush()
+                W1_val, W2_val = sess.run([W1, W2])
+                P_n_val = sess.run(P_n)
+                epoch_loss += (loss_sum / num_repeat)
+            cur_step += 1 
+        cur_epoch += 1
 
     _start_shell(locals())
-
-    f_win.close()
-    f_linux.close()
-    f_arm.close()
 
 if __name__ == '__main__':
     main(sys.argv)
