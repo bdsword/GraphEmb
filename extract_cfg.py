ea70c119b622c23597b5de2f3a5fbe05a3918ea7
BDSword <thetruebloodsword@gmail.com>
2017-12-20 15:21:02 +0800

    Remove useless function name extract function.

--- extract_cfg.py
+++ extract_cfg.py
@@ -9,20 +9,6 @@ import subprocess
 from config import *
 
 
-def extract_func_names(file_path):
-    key_words = ['if', 'for', 'while']
-    with open(file_path, 'r') as f:
-        code = f.read()
-        res = re.findall(r'\w+[\s\n\t]+(\w+)[\s\n\t]*\(.*\)[\s\n\t]*\{', code)
-        if len(res) > 0:
-            func_names = set(res)
-            for key_word in key_words:
-                if key_word in func_names:
-                    func_names.remove(key_word)
-            return func_names 
-    return None
-
-
 def generate_dot(r2agent, fun_name, file_path):
     r2agent.cmd('ag {fun_name} > {binary_path}~{fun_name}.dot'.format(fun_name=fun_name, binary_path=file_path))
     return
@@ -36,7 +22,7 @@ def extract_cfg(file_path):
         items = f.split()
         func_name = items[-1]
         func_addr = items[0]
-        if not re.search("(sym\._)|(sym\.imp)|(sym\.mingw)|(sym\..*cxx::)|(sym\..*std::)|(sym\.register_tm_clones)|(sym\.deregister_tm_clones)|(call_weak_fn)|(operator)", func_name) and re.search("sym\.", func_name):
+        if not re.search("(sym\._)|(sym\.imp)|(sym\.mingw)|(sym\..*cxx::)|(sym\..*std::)|(sym\.register_tm_clones)|(sym\.deregister_tm_clones)|(call_weak_fn)|(operatordelete)|(operatornew)", func_name) and re.search("sym\.", func_name):
             funcs[func_name] = func_addr
 
     for func_name in funcs:
@@ -58,7 +44,6 @@ def main(argv):
     for file_name in files:
         sub_folder = os.path.join(src_folder, file_name)
         src_file_path = os.path.join(sub_folder, file_name) + '.cpp'
-        func_names = extract_func_names(src_file_path)
         for arch in archs:
             binary_file_path = src_file_path + '.' + arch
             if os.path.isfile(binary_file_path):
