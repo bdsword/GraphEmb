d13d1f555ac0d43cc08e08949bdcbbee81a76c31
BDSword <thetruebloodsword@gmail.com>
2018-01-25 15:01:23 +0800

    Change unzip_all.py to single level dir unzipping, and keep an author an dir.

--- unzip_all.py
+++ unzip_all.py
@@ -5,7 +5,7 @@ import os
 import zipfile
 import uuid
 import re
-import glob
+
 
 def main(argv):
     src_folder = argv[1]
@@ -17,8 +17,13 @@ def main(argv):
         print('The output path is not a folder.')
         sys.exit(-2)
 
+    author_max_try = {}
+
+    if not src_folder.endswith('/'):
+        src_folder += '/'
     # List all files in the src_folder
-    for file_path in glob.iglob(src_folder + '**', recursive=True):
+    for name in os.listdir(src_folder):
+        file_path = os.path.join(src_folder, name)
         # Continue if the file is not a file
         if not os.path.isfile(file_path):
             continue
@@ -27,21 +32,25 @@ def main(argv):
         try:
             with open(file_path, 'rb') as fh:
                 zip_file = zipfile.ZipFile(fh)
-                hash_name = str(uuid.uuid4())
-                folder_path = os.path.join(output_folder, hash_name)
-                os.mkdir(folder_path)
-                found_cpp = False
-                for name in zip_file.namelist():
-                    pattern = re.compile('\w+\.cpp')
-                    if pattern.search(name):
-                        found_cpp = True
-                        zip_file.extract(name, folder_path)
-                        os.rename(os.path.join(folder_path, name), os.path.join(folder_path, hash_name) + '.cpp')
-                if not found_cpp:
-                    os.rmdir(os.path.join(output_folder, hash_name))
+                items = re.findall(r'(.*)_(\d+)_(\d+)', os.path.splitext(os.path.basename(file_path))[0])[0]
+                author = items[0]
+                problem_id = int(items[1])
+                author_dir = os.path.join(output_folder, author)
+
+                if not os.path.isfile(author_dir) and not os.path.isdir(author_dir):
+                    os.mkdir(author_dir)
+                name = zip_file.namelist()[0]
+                items = os.path.splitext(name)
+                code_ext = items[1]
+
+                zip_file.extract(name, author_dir)
+                os.rename(os.path.join(author_dir, name),
+                          os.path.join(author_dir, str(problem_id) + code_ext))
 
         except zipfile.BadZipFile as e:
-            os.remove(file_path)
+            print('{} is not a valid zip file.'.format(file_path))
+            print('Maybe you should run the google_codejam_cleaner.py first.')
+            sys.exit(-2)
 
 if __name__ == '__main__':
     main(sys.argv)
