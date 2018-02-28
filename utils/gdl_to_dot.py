c05031558e2318ddb57bbe394a795a7d64211d26
bdsword <thetruebloodsword@gmail.com>
2018-02-28 16:43:08 +0800

    Replace glob to os.walk which is able to iterate hidden files (begin with .).

--- utils/gdl_to_dot.py
+++ utils/gdl_to_dot.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-import glob
 import os
 import argparse
 import subprocess
+import fnmatch
 
 
 parser = argparse.ArgumentParser(description='Transfer the .gdl files in the given path into .dot files.')
 parser.add_argument('TargetDir', help='A folder path to be processed.')
 args = parser.parse_args()
 
-for gdl_file in glob.glob(os.path.join(args.TargetDir, '**', '*.gdl'), recursive=True):
-    dot_file = os.path.splitext(gdl_file)[0] + '.dot'
-    print('>>> Processing {} ...'.format(gdl_file))
-    subprocess.call(['graph-easy', '--input', gdl_file, '--output', dot_file])
+for root, dirnames, filenames in os.walk(args.TargetDir):
+    for filename in fnmatch.filter(filenames, '*.gdl'):
+        full_path = os.path.join(root, filename)
+        dot_file = os.path.splitext(full_path)[0] + '.dot'
+        print('>>> Processing {} ...'.format(full_path))
+        subprocess.call(['graph-easy', '--input', full_path, '--output', dot_file])
 
