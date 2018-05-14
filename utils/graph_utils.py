7d2e145e90699698687de3f7937eee099f552638
bdsword <thetruebloodsword@gmail.com>
2018-05-15 00:28:54 +0800

    Extract get_statistical_features, get_structural_features and create_acfg_from_file to utils/graph_utils.py.

--- utils/graph_utils.py
+++ utils/graph_utils.py
@@ -1,6 +1,8 @@
 import pickle
 import os
 import networkx as nx
+from features.statistical_features import statistical_features
+from features.structural_features import structural_features
 
 
 def extract_main_graph(file_path):
@@ -26,5 +28,35 @@ def read_graph(path):
     else:
         with open(cached_plk, 'rb') as f:
             graph = pickle.load(f)
+    return graph
+
+
+def get_statistical_features(graph, arch):
+    features = dict()
+    for feature_func in statistical_features:
+        features[feature_func.__name__] = dict()
+        for node in graph.nodes:
+            code = graph.nodes[node]['label']
+            features[feature_func.__name__][node] = float(feature_func(code, arch))
+    return features
+
+
+def get_structural_features(graph):
+    features = dict()
+    for feature_func in structural_features:
+        features[feature_func.__name__] = feature_func(graph)
+    return features
+
+
+def create_acfg_from_file(file_path, arch):
+    graph = read_graph(file_path)
+    features_dict = get_statistical_features(graph, arch)
+    for feature_name in features_dict:
+        nx.set_node_attributes(graph, features_dict[feature_name], feature_name)
+
+    feature_dict = get_structural_features(graph)
+    for feature_name in feature_dict:
+        nx.set_node_attributes(graph, feature_dict[feature_name], feature_name)
+
     return graph
 
